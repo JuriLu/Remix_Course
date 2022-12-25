028cff01e1acd9723f1c01c6f121439a7f51955a
Juri <lubonjajurgen1@gmail.com>
2022-12-25 12:33:01 +0100

    Navigation with Link element without making a new request (SPA app)

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,7 +1,10 @@
+import { Link } from "@remix-run/react";
+
 export default function Index() {
   return (
     <>
       <h1>Welcome to Remix</h1>
+      <Link to="/demo">Go to demo page</Link>
     </>
   );
 }
